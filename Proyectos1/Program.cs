9fabc52eada1e26cfb1d6fdec1fa93af320e6eb3
Gama´s Product <Gamas Product@DESKTOP-S14JKI4>
2022-09-02 18:03:14 -0700

    Agregar archivos de proyecto.

---
+++ Proyectos1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyectos1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("mi nombre Alan Contreras");
+            Console.WriteLine("mi edad es 22 años");
+            Console.WriteLine("mexicali, Baja California");
+            Console.ReadKey();
+        }
+    }
+}
